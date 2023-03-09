850bb1c43c4164e4c63b119bc669b12fcf24ad17
Konstantin-Here-Now <coolkonstnow@gmail.com>
2023-03-10 00:25:06 +0300

    Added exchange rates from MOEX with ScrapingAntClient (temporary, because it's not always free).

--- get_data.py
+++ get_data.py
@@ -1,22 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
-
-URL = 'https://cbr.ru'
-request = requests.get(URL)
-soup = BeautifulSoup(request.text, 'html.parser')
+from scrapingant_client import ScrapingAntClient
 
 
-def soup_find_and_get_text(div_class: str, position: int):
+def soup_find_and_get_text(soup: BeautifulSoup, div_class: str, position: int):
     # instead of repeating lines like soup.find_all("div", {'class': "main-indicator_value"})[2].text.rstrip()
     return soup.find_all("div", {'class': div_class})[position].text.rstrip()
 
 
-def get_MOEX_data(data: dict):
-    pass
+def get_moex_data(data):
+    # Define URL with a dynamic web content
+    usd_url = "https://www.moex.com/ru/issue/USD000UTSTOM/CETS"
+    eur_url = "https://www.moex.com/ru/issue/EUR_RUB__TOM/CETS"
+    # Create a ScrapingAntClient instance
+    client = ScrapingAntClient(token='efc9f455d10c40c79cd922533a11ce9e')
+    # Get the HTML page rendered content
+    usd_page_content = client.general_request(usd_url).content
+    eur_page_content = client.general_request(eur_url).content
+    # Parse content with BeautifulSoup
+    usd_soup = BeautifulSoup(usd_page_content, features="lxml")
+    eur_soup = BeautifulSoup(eur_page_content, features="lxml")
+    moex_usd = usd_soup.find("li", {"class": "last", "title": "Курс последней сделки"}).text
+    moex_eur = eur_soup.find("li", {"class": "last", "title": "Курс последней сделки"}).text
+    data['moex_usd'] = moex_usd
+    data['moex_eur'] = moex_eur
+    return data
 
 
 def get_cb_data(data: dict):
-    pass
+    cb_url = 'https://cbr.ru'
+    request = requests.get(cb_url)
+    cb_soup = BeautifulSoup(request.text, 'html.parser')
+    for key in data.keys():
+        data[key] = soup_find_and_get_text(cb_soup, *data[key])
+    return data
 
 
 def main():
@@ -36,7 +53,7 @@ def main():
         }
     )
 
-    for key in data.keys():
-        data[key] = soup_find_and_get_text(*data[key])
+    data = get_cb_data(data)
+    data = get_moex_data(data)
 
     return data
